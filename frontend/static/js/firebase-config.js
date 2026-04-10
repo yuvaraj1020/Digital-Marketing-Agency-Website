e0664c9f8f9d817d780edcb9e9370f77c433c2ba
yuvaraj1020 <yuvarajdevarakonda24@gmail.com>
2026-04-10 15:28:06 +0530

    Migrate to full serverless architecture using Firebase Firestore

---
+++ frontend/static/js/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy, deleteDoc, doc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// You can find this in your Firebase Console -> Project Settings -> General
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export instances to be used in other scripts
+export { db, auth, collection, addDoc, getDocs, query, orderBy, deleteDoc, doc, serverTimestamp };
